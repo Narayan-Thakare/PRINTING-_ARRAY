c2ca3369c8e61890878416214c4a8071afdc1e84
Narayan Thakare <narayan111thakare@gmail.com>
2024-03-09 22:39:36 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		int a[]={3,5,6,7,89};
+		
+		
+		
+		for(int i=0;i<a.length;i++){
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
